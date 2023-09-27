1ec95d991e221e2c66128a0f7bf6fa3d7a49a9bf
Rafi68512 <rafi68512@gmail.com>
2023-09-27 23:41:54 +0700

    feat(Rumus Module): Create NodeJS module to calculate area and perimeter of square and rectangle

---
+++ Week 7/rumus.js
@@ -0,0 +1,21 @@
+// Module NodeJS
+
+// Menghitung luas persegi
+eksports.luasPersegi = (sisi) => {
+  return sisi * sisi;
+};
+
+// Menghitung keliling persegi
+eksports.kelilingPersegi = (sisi) => {
+  return 4 * sisi;
+};
+
+// Menghitung luas persegi panjang
+eksports.luasPersegiPanjang = (panjang, lebar) => {
+  return panjang * lebar;
+};
+
+// Menghitung keliling persegi panjang
+eksports.kelilingPersegiPanjang = (panjang, lebar) => {
+  return 2 * (panjang + lebar);
+};
