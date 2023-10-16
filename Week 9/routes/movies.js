b0affc53e0914f628190f16eb55d26f65143cf34
Rafi68512 <rafi68512@gmail.com>
2023-10-16 14:58:26 +0700

    (File)

--- Week 9/routes/movies.js
+++ Week 9/routes/movies.js
@@ -116,12 +116,14 @@ var router = express.Router();
 
 var pool = require("../db/query.js");
 
-// GET Movies
+// GET Movies With Pagination
 router.get("/", function (req, res) {
+  const { page = 1, limit = 10 } = req.query;
+  const offset = (page - 1) * limit;
+
   pool.query(
-    `SELECT * FROM movies ${
-      req.query.limit ? "LIMIT " + req.query.limit : ""
-    } `,
+    `SELECT * FROM movies LIMIT $1 OFFSET $2`,
+    [limit, offset],
     (error, results) => {
       if (error) {
         throw error;
@@ -191,5 +193,4 @@ router.put("/:id", function (req, res) {
   );
 });
 
-//export this router to use in our index.js
 module.exports = router;
